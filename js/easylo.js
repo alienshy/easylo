31bc8d932fe01b1179e87099c125f674a7459507
alienshy <alisahverdiyeW8@gmail.com>
2023-12-22 14:13:25 +0400

    commited

--- js/easylo.js
+++ js/easylo.js
@@ -1,11 +1,11 @@
 window.addEventListener("scroll",()=>{
     if(window.scrollY>800){
-        document.querySelector("nav").style.position = "fixed"
+        document.querySelector("nav").style.backgroundColor = "#10C98F"
         document.querySelector("nav").style.top="0"
         document.querySelector("nav").style.left="0"
     }
     else{   
-        document.querySelector("nav").style.position = ""
+        document.querySelector("nav").style.backgroundColor = ""
         document.querySelector("nav").style.top=""
         document.querySelector("nav").style.left=""
     }
